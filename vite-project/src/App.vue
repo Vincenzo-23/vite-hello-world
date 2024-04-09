455003f249dd3fccca35747c1857ab4c9b3d5ac1
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-04-09 14:56:36 +0200

    Fix: fixed the components and used to create the app

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,24 +1,23 @@
-<script>
-export default {
-  data() {
-    return {
-      message: `La mia prima app con Vite!`,
-    }
-  }
-
-}
+<script setup>
+import AppTitle from './components/AppTitle.vue';
+import HelloWorld from './components/AppTitle.vue'
 </script>
 
 <template>
-    <h1 class="title">
-       {{ message }} 
-    </h1>
+  <AppTitle/>
 </template>
 
-<style>
-
-.title{
-  color: orange;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      message: `La mia prima app con Vite!`,
+    }
+  }
+
+}
+</script>
+
+<template>
+    <h1 class="title">
+       {{ message }} 
+    </h1>
+</template>
+
+<style>
+
+.title{
+  color: orange;
+}
+
+</style>
